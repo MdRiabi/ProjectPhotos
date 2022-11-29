73a7735c3663686e2054886dacf032b16704258a
Mohamed Riabi <mhdriabi2013@gmail.com>
2022-11-29 14:29:12 +0100

    change for enable angular module imports like httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoShowComponent } from './photo-show/photo-show.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { PhotoShowComponent } from './photo-show/photo-show.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
 }
